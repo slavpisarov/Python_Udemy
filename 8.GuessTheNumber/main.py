0d27c515ab2317e178fd36d6091fc2557deeb340
Slav Pisarov <slav.pisarov@gmail.com>
2024-01-11 16:50:38 +0200

    Complete guess the number game

--- 8.GuessTheNumber/main.py
+++ 8.GuessTheNumber/main.py
@@ -1,10 +1,36 @@
-import random
+from random import randint
 
-print("I'm thinking of a number between 1 and 100.")
-choice = input("Choose difficulty level. Type 'easy' or 'hard' : ")
+def check_answer(user_choice,computer_choce, attempts ):
+    if user_choice < computer_choce:
+        print("Too low.")
+        return attempts - 1
+    elif user_choice > computer_choce:
+        print("Too high.")
+        return attempts - 1
+    else:
+        print("Correct! You Win!")
+    
+def game():
+    print("I'm thinking of a number between 1 and 100.")
+    difficulty = input("Choose difficulty level. Type 'easy' or 'hard' : ")
 
-attempts: 0
-if choice == 'easy':
-    attempts = 10
-else:
-    attempts = 5
+    attempts: 0
+    if difficulty == 'easy':
+        attempts = 10
+    else:
+        attempts = 5
+
+    computer_choce = randint(0,100)
+
+    guess = 0;
+    while guess != computer_choce :
+        print(f"You have {attempts} attempts to guess the number.")
+        guess = int(input("Make a guess: "))
+
+        attempts = check_answer(guess, computer_choce, attempts)
+
+        if(attempts == 0):
+            print("You are out of turns, you loose.")
+            return
+
+game()
